0b663a8dcda80a6aa27ec726e0e62ba3033f8367
gokulg001 <gokuljune2003@gmail.com>
2022-11-10 10:20:25 +0530

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,11 +1,16 @@
 import unittest
-from translator import englishToFrench,frenchToEnglish
+from translator import english_to_french,french_to_english
 
 class TestMyModule(unittest.TestCase):
-    def test_englishToFrence(self):
-        self.assertEqual(englishToFrench('Hello'),'Bonjour')
-    def test_frenchToEnglish(self):
-        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french(''),' ')
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english(''),' ')
+
+    def test_english_to_french(self):
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
+    def test_french_to_english(self):
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
 
 if __name__=='__main__':
     unittest.main()
